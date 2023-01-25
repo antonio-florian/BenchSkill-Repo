78fc9de92c44bf46f68788be2ee3dd1a6973e52c
antonio-florian <antonyo.florian@gmail.com>
2023-01-25 07:13:31 +0200

    added validation for create product

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-product',
@@ -7,9 +9,81 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./create-product.component.css'],
 })
 export class CreateProductComponent implements OnInit {
-  constructor() {}
+  createForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
-  onSubmit() {}
+  ngOnInit(): void {
+    this.createForm = this.formBuilder.group({
+      name: [
+        '',
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Z ]*$'),
+      ],
+      Description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.pattern('^[a-zA-Z ]*$'),
+        ],
+      ],
+      Price: ['', Validators.required],
+      Category: ['', Validators.required],
+      Image_URL: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      Select: ['', Validators.required],
+    });
+  }
+  get name() {
+    return this.createForm.get('name');
+  }
+
+  get Description() {
+    return this.createForm.get('Description');
+  }
+
+  get Price() {
+    return this.createForm.get('Price');
+  }
+
+  get Category() {
+    return this.createForm.get('Category');
+  }
+
+  get Image_URL() {
+    return this.createForm.get('Image_URL');
+  }
+
+  get phoneNumber() {
+    return this.createForm.get('phoneNumber');
+  }
+
+  get Select() {
+    return this.createForm.get('Select');
+  }
+
+  onSubmit() {
+    if (this.createForm.valid) {
+      this.http
+        .post<any>('http://localhost:5000/Products', this.createForm.value)
+        .subscribe(
+          (res) => {
+            alert('Succesfuly Registered');
+            this.createForm.reset();
+            this.router.navigate(['product-list-component']);
+          },
+          (err) => {
+            alert('Something went wrong');
+          }
+        );
+    } else {
+      alert('createForm not valid');
+    }
+  }
 }
